fdfb13ea5f337b04c75167f901c0c16e9b629a45
ealbinu <ealbinu@gmail.com>
2023-02-12 13:52:27 -0600

    3 files modified

--- src/components/RequestPrediales.vue
+++ src/components/RequestPrediales.vue
@@ -1,8 +1,21 @@
 <template>
   <p>Cuenta: {{ cuenta }}</p>
+  <template v-for="(item, index) from vencidos">
+    <a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
+  </template>
 </template>
 
 <script setup>
 import axios from 'axios';
 const props = defineProps(['cuenta']);
+const vencidos  = []
+
+const loads = async () => {
+  vencidos = await axios.get(
+    'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public/api/v1/adeudos/vencido/' +
+      props.cuenta
+  );
+}
+
+loads ();
 </script>
