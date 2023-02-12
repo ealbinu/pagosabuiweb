cb811c1304059c6ce67ed545a86fece98475cdf1
ealbinu <ealbinu@gmail.com>
2023-02-12 14:11:25 -0600

    3 files modified

--- src/components/RequestPrediales.vue
+++ src/components/RequestPrediales.vue
@@ -1,33 +1,50 @@
 <template>
   <p>Cuenta: {{ cuenta }}</p>
-  <template v-for="(item, index) of vencidos">
-    <CoreArrow variant="path" strokeWidth="6" , flip="x"></CoreArrow>
-    ><a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
-  </template>
+  <div class="loading" v-if="loading">Cargando...</div>
+  <div class="porpagar">
+    <div>
+      <h4>Vencidos</h4>
+      <!--
+      <template v-for="(item, index) of vencidos">
+        ><a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
+      </template>
+      -->
+    </div>
+    <div>
+      <h4>Vigentes</h4>
+      <!--
+      <template v-for="(item, index) of vencidos">
+        ><a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
+      </template>
+      -->
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import axios from 'axios';
-import { CoreFigma } from '@glyphs/vue-core';
 
 const props = defineProps(['cuenta']);
-const vencidos = [];
+const loading = ref(true);
+const vencidos = ref([]);
 
 const loads = async () => {
-  console.log('loading...');
+  let api = 'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public/api/v1';
+  const dataVen = await .get(api+'/adeudos/vencido/' + props.cuenta );
+  const dataVig = await .get(api+'/adeudos/vigente/' + props.cuenta );
+  /*
   axios
     .get(
       'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public/api/v1/adeudos/vencido/' +
         props.cuenta
     )
     .then((res) => {
-      console.log(res);
-      // Handle response
+      vencidos = res.data;
     })
     .catch((error) => {
       console.log(error.response);
     });
-};
-
-loads();
+    */
+}
 </script>
