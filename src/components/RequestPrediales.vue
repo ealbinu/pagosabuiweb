90b748a26ace760fb5208ff3481c3ab44a856ab3
ealbinu <ealbinu@gmail.com>
2023-02-12 14:02:55 -0600

    3 files modified

--- src/components/RequestPrediales.vue
+++ src/components/RequestPrediales.vue
@@ -1,21 +1,33 @@
 <template>
   <p>Cuenta: {{ cuenta }}</p>
-  <template v-for="(item, index) from vencidos">
-    <a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
+  <template v-for="(item, index) of vencidos">
+    <CoreArrow variant="path" strokeWidth="6" , flip="x"></CoreArrow>
+    ><a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
   </template>
 </template>
 
 <script setup>
 import axios from 'axios';
+import { CoreFigma } from '@glyphs/vue-core';
+
 const props = defineProps(['cuenta']);
-const vencidos  = []
+const vencidos = [];
 
 const loads = async () => {
-  vencidos = await axios.get(
-    'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public/api/v1/adeudos/vencido/' +
-      props.cuenta
-  );
-}
+  console.log('loading...');
+  axios
+    .get(
+      'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public/api/v1/adeudos/vencido/' +
+        props.cuenta
+    )
+    .then((res) => {
+      console.log(res);
+      // Handle response
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+};
 
-loads ();
+loads();
 </script>
