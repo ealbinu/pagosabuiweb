6cae791d5a3d8e7095846b4b535bac24cc901dce
ealbinu <ealbinu@gmail.com>
2023-02-12 18:52:17 -0600

    2 files modified

--- src/components/RequestPrediales.vue
+++ src/components/RequestPrediales.vue
@@ -1,24 +1,31 @@
 <template>
   <p>Cuenta: {{ cuenta }}</p>
   <div class="loading" v-if="loading" aria-busy="true">Cargando...</div>
-  <div class="grid" v-else>
-    <div>
-      <h6>Vencidos</h6>
-      {{ vencidos }}
+  <article>
+    <h6>Vencidos</h6>
+    <table>
       <template v-for="(item, index) of vencidos">
-        <a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
+        <tr>
+          <td>{{ item.EJERCICIO }}</td>
+          <td>{{ item.PERIODO }}</td>
+          <td>{{ item.IMPORTEADEUDO }}</td>
+          <td>{{ item.IMPORTEPAGADO }}</td>
+          <td><a href="#">Formato de pago</a></td>
+        </tr>
       </template>
-      <div v-if="!vencidos">Sin vencidos</div>
-    </div>
-    <div>
-      <h6>Vigentes</h6>
-      {{ vigentes }}
-      <template v-for="(item, index) of vigentes">
-        <a href="#"> {{ item.ano }}/{{ item.periodo }}</a>
-      </template>
-      <div v-if="!vigentes">Sin vigentes</div>
-    </div>
-  </div>
+    </table>
+    <div v-if="!vencidos">Sin vencidos</div>
+  </article>
+  <article>
+    <h6>Vigentes</h6>
+    <template v-for="(item, index) of vigentes">
+      <tr>
+        <td><a href="#">Formato pago</a></td>
+      </tr>
+      <a href="#"> {{ item.texto_bimestre }}</a>
+    </template>
+    <div v-if="!vigentes">Sin vigentes</div>
+  </article>
 </template>
 
 <script setup>
@@ -34,13 +41,13 @@ const loads = async () => {
   let api = 'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public/api/v1';
 
   try {
-    vigentes.value = await axios.get(api + '/adeudos/vigente/' + props.cuenta)
-      .data;
+    const res = await axios.get(api + '/adeudos/vigente/' + props.cuenta);
+    vigentes.value = res.data;
     console.log('::VIFGENTE:::', vigentes.value);
   } catch {}
   try {
-    vencidos.value = await axios.get(api + '/adeudos/vencido/' + props.cuenta)
-      .data;
+    const res = await axios.get(api + '/adeudos/vencido/' + props.cuenta);
+    vencidos.value = res.data;
     console.log('VENCIDO:::::', vencidos.value);
   } catch {}
   loading.value = false;
