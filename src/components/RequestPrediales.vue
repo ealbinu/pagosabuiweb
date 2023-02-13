e1002da1a38ee9120e393d807a5f8cf448b74bdc
ealbinu <ealbinu@gmail.com>
2023-02-12 19:06:03 -0600

    1 file modified

--- src/components/RequestPrediales.vue
+++ src/components/RequestPrediales.vue
@@ -1,26 +1,26 @@
 <template>
   <p>Cuenta: {{ cuenta }}</p>
   <div class="loading" v-if="loading" aria-busy="true">Cargando...</div>
-  <article>
-    <h6>Vencidos</h6>
-    <table>
-      <template v-for="(item, index) of vencidos">
-        <tr>
-          <td>{{ item.EJERCICIO }}</td>
-          <td>{{ item.PERIODO }}</td>
-          <td>{{ item.IMPORTEADEUDO }}</td>
-          <td>{{ item.IMPORTEPAGADO }}</td>
-          <td><a href="#">Formato de pago</a></td>
-        </tr>
-      </template>
-    </table>
-    <div v-if="!vencidos">Sin vencidos</div>
-  </article>
+
+  <h6>Vencidos</h6>
+  <table>
+    <template v-for="(item, index) of vencidos">
+      <tr>
+        <td>
+          <strong>{{ item.ANO }}/{{ item.PERIODO }}</strong>
+        </td>
+        <td>${{ item.SALDO }}</td>
+        <td><a href="#" @click="formato(item)"> Formato</a></td>
+      </tr>
+    </template>
+  </table>
+  <div v-if="!vencidos">Sin vencidos</div>
+
   <article>
     <h6>Vigentes</h6>
     <template v-for="(item, index) of vigentes">
       <tr>
-        <td><a href="#">Formato pago</a></td>
+        <td><a href="#" @click="formato(item)">Formato pago</a></td>
       </tr>
       <a href="#"> {{ item.texto_bimestre }}</a>
     </template>
@@ -31,6 +31,7 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+const api = 'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public';
 
 const props = defineProps(['cuenta']);
 const loading = ref(true);
@@ -38,19 +39,38 @@ const vencidos = ref(null);
 const vigentes = ref(null);
 
 const loads = async () => {
-  let api = 'https://ovica.finanzas.cdmx.gob.mx/ovica-backend/public/api/v1';
-
   try {
-    const res = await axios.get(api + '/adeudos/vigente/' + props.cuenta);
+    const res = await axios.get(
+      api + '/api/v1/adeudos/vigente/' + props.cuenta
+    );
     vigentes.value = res.data;
-    console.log('::VIFGENTE:::', vigentes.value);
+
   } catch {}
   try {
-    const res = await axios.get(api + '/adeudos/vencido/' + props.cuenta);
+    const res = await axios.get(
+      api + '/api/v1/adeudos/vencido/' + props.cuenta
+    );
     vencidos.value = res.data;
-    console.log('VENCIDO:::::', vencidos.value);
+
   } catch {}
   loading.value = false;
 };
 loads();
+
+const formato = async (item) => {
+  const res = await axios.patch(api + '/api/v1/adeudos/vencido/liquidacion', {
+    adeudos: [{ ...item, checked: true }],
+    ctapredial: props.cuenta,
+  });
+  const formato = await axios.post(api + '/formatos/pago/generaFUT', {
+    token: res.data.resumen.lc_token,
+  });
+  console.log(formato);
+  /*
+  const res = axios.post('/formatos/pago/generaFUT', {
+    token: token,
+  });
+  */
+  console.log(res);
+};
 </script>
